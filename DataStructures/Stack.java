0c77ca516febd62a861dabb637ff201deee5d5c6
Manish-Baghel29 <mbaghel699@gmail.com>
2025-03-01 13:28:54 +0530

    Stack operations

--- DataStructures/Stack.java
+++
@@ -1,9 +0,0 @@
-// Stack implementation using ArrayList
-import java.util.*;
-public class Stack {
-    ArrayList<Integer> list=new ArrayList<>();
-    public static boolean isEmpty()
-    public static void main(String[] args) {
-        
-    }
-}
