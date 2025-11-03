d1c99ec8ee7228ebe8b9360bc8c62eaabe13e04b
Manish Baghel <mbaghel699@gmail.com>
2025-11-03 23:29:47 +0530

    Started tree

---
+++ DSA/BinaryTrees/BuildPreOrderTree.java
@@ -0,0 +1,105 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BuildPreOrderTree {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node root = new Node(nodes[idx]);
+            root.left = buildTree(nodes);
+            root.right = buildTree(nodes);
+
+            return root;
+        }
+    }
+
+    // PreOrder Traversal
+    public static void PreOrder(Node root){
+        if(root == null){
+            return ;
+        }
+        System.out.print(root.data + " ");
+        PreOrder(root.left);
+        PreOrder(root.right);
+    }
+
+    // InOrder Traversal
+    public static void inOrder(Node root){
+        if(root == null){
+            return ;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    // PostOrder Traversal
+    public static void postOrder(Node root){
+        if(root == null){
+            return ;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    // LevelOrder Traversal
+    public static void levelOrder(Node root){
+        if(root == null){
+            return ;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node currNode = q.remove();
+            if(currNode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                System.out.print(currNode.data+" ");
+                if(currNode.left != null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right != null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        // BinaryTree tree = new BinaryTree();
+        Node root = BuildPreOrderTree.BinaryTree.buildTree(nodes);
+        System.out.println(root.data);
+        PreOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+        System.out.println();
+        levelOrder(root);
+    }
+}
--- Web Dev/Backend/dbRelationships/Models/posts.js
+++ Web Dev/Backend/dbRelationships/Models/posts.js
@@ -22,42 +22,58 @@ const userSchema = new Schema({
     email: String,
 });
 
+// userSchema.pre('findOneAndDelete' , async()=>{
+//     console.log('Pre middleware and user is');
+// });
+
+userSchema.post('findOneAndDelete' , async(user)=>{
+    let res = await Post.deleteMany({user: user});
+    console.log(res);
+});
+
 const User = mongoose.model('User', userSchema);
 const Post = mongoose.model('Post',postSchema);
 
-// const addUser = async () => {
-//     await User.findOneAndDelete({username:'manishbaghel'});
-//     let result = await User.insertOne({
-//         username: 'manishbaghel',
-//         email: 'mbaghel@gmail.com',
-//     });
-//     console.log(result);
-// };
-
-// const addPost = async()=>{
-//     await Post.deleteMany({});
-//     let res = await Post.insertMany([
-//         {
-//             content:"Breaking Bad Story!",
-//             likes:75000,
-//             user:await User.findOne({username : "manishbaghel"}),
-//         },
-//         {
-//             content:"GOT Story!",
-//             likes:73000,
-//             user:await User.findOne({username : "manishbaghel"}),
-//         },
-//     ]);
-
-//     console.log(res);
-// };
-
-// addUser();
-// addPost();
-
-async function findPosts(){
-    let result = await Post.find({});
+const addUser = async () => {
+    await User.findOneAndDelete({username:'manishbaghel'});
+    let result = await User.insertOne({
+        username: 'manishbaghel',
+        email: 'mbaghel@gmail.com',
+    });
     console.log(result);
-}
+};
 
-findPosts();
+const addPost = async()=>{
+    await Post.deleteMany({});
+    let res = await Post.insertMany([
+        {
+            content:"Breaking Bad Story!",
+            likes:75000,
+            user:await User.findOne({username : "manishbaghel"}),
+        },
+        {
+            content:"GOT Story!",
+            likes:73000,
+            user:await User.findOne({username : "manishbaghel"}),
+        },
+    ]);
+
+    console.log(res);
+};
+
+addUser();
+addPost();
+
+// async function findPosts(){
+//     let result = await Post.find({});
+//     console.log(result);
+// }
+
+// findPosts();
+
+const delUser = async ()=>{
+    let res= await User.findByIdAndDelete('68ab2190bde05ca2c5dd8f8b'); //replace id accordingly.
+    console.log(res);
+};
+
+delUser();
